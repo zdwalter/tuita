bd4b1680f7d1e17584df0ed6f1e53c575411a85d
Walter Zheng <zdwalter@gmail.com>
2011-05-09 14:40:08 +0800

    can login!

--- assets/js/tuita.js
+++ assets/js/tuita.js
@@ -6,13 +6,21 @@ var cookie = '';
 
 function check_cookie() {
     //Set-Cookie: SDO_ACCOUNT_TYPE=; domain=.sdo.com; path=/
-    alert("responseHeader: "+ ajax_handler.getAllResponseHeaders());
+    //alert("responseHeader: "+ ajax_handler.getAllResponseHeaders());
     var reg = /Set-Cookie: .*/;
     var result = reg.exec(ajax_handler.getAllResponseHeaders());
     eval((""+result).replace('Set-Cookie: ',"cookie += ';")+"'");
     //alert(cookie);
 
 };
+
+function reset_cookie_from_response() {
+    var reg = /Set-Cookie: .*/;
+    var result = reg.exec(ajax_handler.getAllResponseHeaders());
+    eval((""+result).replace('Set-Cookie: ',"cookie = '")+"'");
+    //alert(cookie);
+};
+
 function doLoad() {
     theData.get('login', 
         function(theLogin) {
@@ -29,7 +37,6 @@ function doLoad() {
 }
 
 function doLogin_ticket() {
-    var c = 'b_t_s=t104727701073xs; __utmz=143606314.1304858611.3.7.utmcsr=login.sdo.com|utmccn=(referral)|utmcmd=referral|utmcct=/sdo/Logout/appLogout.php; PHPSESSID=39f73ce0e7dcec9e708a5cc483bc7428; __utma=143606314.1618934333.1304846355.1304858611.1304863150.4; __utmc=143606314; __utmb=143606314.6.10.1304863150';
     var c = 'b_t_s=t104727701073xs; __utmz=143606314.1304858611.3.7.utmcsr=login.sdo.com|utmccn=(referral)|utmcmd=referral|utmcct=/sdo/Logout/appLogout.php; PHPSESSID=39f73ce0e7dcec9e708a5cc483bc7428; __utma=143606314.1618934333.1304846355.1304858611.1304863150.4; __utmc=143606314; __utmb=143606314.7.10.1304863150; tt_login=1; tt_reg=1';
   ajax_handler = $.ajax({
         url: 'http://www.tuita.com/login?refer=&ticket=ST-7dafc45c-f5a5-4640-8e7e-6583d552421c',
@@ -45,23 +52,23 @@ function doLogin_ticket() {
 
 
 };
+function doLogin() {
+    return doLogin_full();
+    //return doLogin_ttst();
+};
 function doLogin_ttst() {
     ajax_handler = $.ajax({
         url: 'http://www.tuita.com/home/getfeed',
-        headers: { Cookie: ' __ttst=5IuTnAl4giTvgu-KxgrWspObgrAyKUFPUUX1rsrNxpSEE4tWdYaXY0M8O2%2A0GCu4H-wQD1ITsL0wmDDao6ep%2A0kNJN8-7ocylEc0wEJ6j187Oa9WjAFhCUrGLUcCgGC2lmfi3Kco7yXKNEQykdDqYQirj3VA90giT5ndUcbxiUY70LXHGexXLciiUfKeosxWT%2A-UV7QjCYk;'},
-        //url: 'https://cas.sdo.com/cas/loginStateService?method=checkstat',
+        headers: { Cookie: '__ttst=ivwJ6PFI8Ra2-4Y9RAiuRRrGFaVWhLDgc5Up9cA4qZBPpa9IoF1bJyePkCKyERwJcgoWjIpYuVSbAI18hqBaKl5UU68bUR1KexbUjeZj5qYNrshjqH9UUaK1CMe%2ALyPPc7NAG8WIlyF3jyF83nlBbF%2A9T7HMohYJAj3FqWDtsGtFnItSYaWskeuxU46s7q%2Ant6thSG94BsA;'},
         type: 'get',
         success: after_getfeed
         });
 
 };
 
-function doLogin() {
+function doLogin_full() {
     var url = "https://dplogin.sdo.com/dispatchlogin.fcgi";
-    //var cookie_str = 'sdo_beacon_id=58.215.45.151.1302607175421.9; __utmz=237578050.1302607176.1.1.utmccn=(direct)|utmcsr=(direct)|utmcmd=(none); ver=1; __utma=237578050.48023496.1302607176.    1302607176.1304689726.2; SNDA_ADRefererSystem_MachineTicket=98cfe54b-c697-42ff-93bd-67afc31f9fa1; sto-id-20480=JMAKJGAKFAAA; sdo_dw_track="xHNvo2V0fNbE4/1D8QURAw==";              SDO_ACCOUNT_TYPE=';
-    //cookie += cookie_str;
     // Retrieve the values from the form elements
-
     var postStr = "";
     x$("input[type=hidden]").each(function(el) {
         postStr += el.name+'='+escape(el.value)+"&";
@@ -89,7 +96,6 @@ function doLogin() {
 
 
 function after_login_sdo(responseText) { 
-    check_cookie();
     this.responseText = responseText;
     //alert("after_login_sdo:responseText: "+this.responseText);
     if(this.responseText == "password missed") { // wrong pass/user!
@@ -109,27 +115,26 @@ function after_login_sdo(responseText) {
         ajax_handler = $.ajax({
             url: href, 
             type: 'get', 
-            headers: { Cookie: cookie },
+     //       headers: { Cookie: cookie },
             success: after_login_sdo_href
         });
     }
 }; //after_login_sdo
 
 function after_login_sdo_href(responseText) {
-    check_cookie();
     this.responseText = responseText;
     //this.responseText = '<script language="javascript">\n document.location = "http://www.tuita.com/login?refer=&ticket=ST-c96505a7-9f18-4045-b817-2c43e6f021cb";    </script>';
     //alert("after_login_sdo_href:"+this.responseText);
     var reg = /ticket=.*;/;
     var result =  reg.exec(this.responseText);
     result = "var href=\"http://zdwalter.tuita.com/?"+result; //FIXME: any other page will cause 302 redirect, which loss cookie by $.ajax
-    alert(result);
     eval(result);
 
-    alert(href);
-    return;
+    var protal_host = 'localhost'; // p.gfw4.info
+    var url = 'http://'+protal_host+"/redirect?url="+escape(href);
+    alert(url);
     ajax_handler = $.ajax({
-        url: href, 
+        url: url, 
         type: 'get', 
         headers: { Cookie: " tt_reg=1; tt_login=1; b_t_s=t104867177621xs; __utmz=143606314.1304867178.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=143606314.288821922.1304867178.1304867178.1304867178.1; __utmc=143606314; __utmb=143606314.3.10.1304867178" },
         success: after_login_tuita,
@@ -137,26 +142,22 @@ function after_login_sdo_href(responseText) {
 }; //after_login_sdo_href
 
 function after_login_tuita(responseText) {
-    check_cookie();
+    reset_cookie_from_response();
     this.responseText = responseText;
-
-    var reg = /__ttst=[^;]*;/;
-    var result = reg.exec(cookie);
-    alert('got __ttst:', result);
-
+    cookie = cookie.replace(/.*__ttst=/,'__ttst=').replace(/;.*/,'');
+    alert('cookie:'+cookie);
     ajax_handler = $.ajax({
         url: 'http://www.tuita.com/home/getfeed',
-        headers: { Cookie: result},
+        headers: { Cookie: cookie},
         type: 'get',
         success: after_getfeed
         });
 }; //after_login_tuita
 
 function go_checkstat(responseText) {
-    check_cookie();
     this.responseText = responseText;
 
-    alert("go_checkstat:"+this.responseText);
+    //alert("go_checkstat:"+this.responseText);
     ajax_handler = $.ajax({
         headers: { Cookie: cookie },
         url: 'https://cas.sdo.com/cas/loginStateService?method=checkstat',
@@ -167,9 +168,9 @@ function go_checkstat(responseText) {
 };
 
 function after_checkstat(stat) {
-    alert(stat);
+    //alert(stat);
     if (stat == "checkstat({'CAS_LOGIN_STATE':'1'})") {
-        alert('login success!');
+        //alert('login success!');
         ajax_handler = $.ajax({
         url: 'http://www.tuita.com/home/getfeed',
         type: 'get',
@@ -181,5 +182,4 @@ function after_checkstat(stat) {
 
 function after_getfeed(feed) {
     alert('feed:\n'+feed);
-    alert(ajax_handler.getAllResponseHeaders());
 };
