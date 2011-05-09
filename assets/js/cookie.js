a6cdc0861de1e3e7c44ab31d72b61f5557fddb0e
Walter Zheng <zdwalter@gmail.com>
2011-05-09 09:55:51 +0800

    move to server side login!

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,45 +1,4 @@
 /**
- * Cookie plugin
- *
- * Copyright (c) 2006 Klaus Hartl (stilbuero.de)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- */
-
-/**
- * Create a cookie with the given name and value and other optional parameters.
- *
- * @example $.cookie('the_cookie', 'the_value');
- * @desc Set the value of a cookie.
- * @example $.cookie('the_cookie', 'the_value', { expires: 7, path: '/', domain: 'jquery.com', secure: true });
- * @desc Create a cookie with all available options.
- * @example $.cookie('the_cookie', 'the_value');
- * @desc Create a session cookie.
- * @example $.cookie('the_cookie', null);
- * @desc Delete a cookie by passing null as value. Keep in mind that you have to use the same path and domain
- *       used when the cookie was set.
- *
- * @param String name The name of the cookie.
- * @param String value The value of the cookie.
- * @param Object options An object literal containing key/value pairs to provide optional cookie attributes.
- * @option Number|Date expires Either an integer specifying the expiration date from now on in days or a Date object.
- *                             If a negative value is specified (e.g. a date in the past), the cookie will be deleted.
- *                             If set to null or omitted, the cookie will be a session cookie and will not be retained
- *                             when the the browser exits.
- * @option String path The value of the path atribute of the cookie (default: path of page that created the cookie).
- * @option String domain The value of the domain attribute of the cookie (default: domain of page that created the cookie).
- * @option Boolean secure If true, the secure attribute of the cookie will be set and the cookie transmission will
- *                        require a secure protocol (like HTTPS).
- * @type undefined
- *
- * @name $.cookie
- * @cat Plugins/Cookie
- * @author Klaus Hartl/klaus.hartl@stilbuero.de
- */
-
-/**
  * Get the value of a cookie with the given name.
  *
  * @example $.cookie('the_cookie');
@@ -53,25 +12,8 @@
  * @cat Plugins/Cookie
  * @author Klaus Hartl/klaus.hartl@stilbuero.de
  */
-var my_cookie = {};
-jQuery.cookie_load = function(cookie_str) {
-    var cookies = cookie_str.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = jQuery.trim(cookies[i]);
-        var set = cookie.split('=', 1);
-        var name = set[0];
-        my_cookie[name] = cookie.substring(name.length+1);
-    }
-};
+var my_cookie_str = '';
 jQuery.cookie = function(name, value, options) {
-    if (typeof name == 'undefined') {
-        var cookie = '';
-        for (key in my_cookie) {
-            value = my_cookie[key];
-            cookie += [key, "=", value].join("") + "; ";
-        };
-        return cookie;
-    }
     if (typeof value != 'undefined') { // name and value given, set cookie
         options = options || {};
         if (value === null) {
@@ -87,16 +29,28 @@ jQuery.cookie = function(name, value, options) {
             } else {
                 date = options.expires;
             }
-            expires = date.toUTCString(); // use expires attribute, max-age is not supported by IE
+            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
         }
         // CAUTION: Needed to parenthesize options.path and options.domain
         // in the following expressions, otherwise they evaluate to undefined
         // in the packed version for some reason...
-        if (name in ['expires', 'path', 'domain', 'secure']) 
-            my_cookie[name] = encodeURIComponent(value);
-        else 
-            my_cookie[name] = value;
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
+        var secure = options.secure ? '; secure' : '';
+        my_cookie_str = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
     } else { // only name given, get cookie
-        return my_cookie[name];
+        var cookieValue = null;
+        if (my_cookie_str && my_cookie_str != '') {
+            var cookies = my_cookie_str.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
     }
 };
--- assets/js/tuita.js
+++ assets/js/tuita.js
@@ -1,15 +1,17 @@
 var theData = new Lawnchair({adaptor:'dom'});
 var ajax_handler = null; 
+var cookie = ''; 
+//key:   -> ticket
+//key: /login?ticket -> __ttst
 
 function check_cookie() {
     //Set-Cookie: SDO_ACCOUNT_TYPE=; domain=.sdo.com; path=/
     alert("responseHeader: "+ ajax_handler.getAllResponseHeaders());
     var reg = /Set-Cookie: .*/;
     var result = reg.exec(ajax_handler.getAllResponseHeaders());
-    eval((""+result).replace('Set-Cookie: ',"var cookie_str = '")+"'");
+    eval((""+result).replace('Set-Cookie: ',"cookie += ';")+"'");
+    //alert(cookie);
 
-    $.cookie_load(cookie_str);
-    alert($.cookie());
 };
 function doLoad() {
     theData.get('login', 
@@ -25,9 +27,39 @@ function doLoad() {
         } // function(theSettings)
 );
 }
- 
+
+function doLogin_ticket() {
+    var c = 'b_t_s=t104727701073xs; __utmz=143606314.1304858611.3.7.utmcsr=login.sdo.com|utmccn=(referral)|utmcmd=referral|utmcct=/sdo/Logout/appLogout.php; PHPSESSID=39f73ce0e7dcec9e708a5cc483bc7428; __utma=143606314.1618934333.1304846355.1304858611.1304863150.4; __utmc=143606314; __utmb=143606314.6.10.1304863150';
+    var c = 'b_t_s=t104727701073xs; __utmz=143606314.1304858611.3.7.utmcsr=login.sdo.com|utmccn=(referral)|utmcmd=referral|utmcct=/sdo/Logout/appLogout.php; PHPSESSID=39f73ce0e7dcec9e708a5cc483bc7428; __utma=143606314.1618934333.1304846355.1304858611.1304863150.4; __utmc=143606314; __utmb=143606314.7.10.1304863150; tt_login=1; tt_reg=1';
+  ajax_handler = $.ajax({
+        url: 'http://www.tuita.com/login?refer=&ticket=ST-7dafc45c-f5a5-4640-8e7e-6583d552421c',
+        headers: { 
+            Cookie: c, 
+            Host: 'www.tuita.com',
+            Referer: 'http://cas.sdo.com/cas/login?service=http%3A%2F%2Fwww.tuita.com'
+            },
+        //url: 'https://cas.sdo.com/cas/loginStateService?method=checkstat',
+        type: 'get',
+        success: go_checkstat 
+        });
+
+
+};
+function doLogin_ttst() {
+    ajax_handler = $.ajax({
+        url: 'http://www.tuita.com/home/getfeed',
+        headers: { Cookie: ' __ttst=5IuTnAl4giTvgu-KxgrWspObgrAyKUFPUUX1rsrNxpSEE4tWdYaXY0M8O2%2A0GCu4H-wQD1ITsL0wmDDao6ep%2A0kNJN8-7ocylEc0wEJ6j187Oa9WjAFhCUrGLUcCgGC2lmfi3Kco7yXKNEQykdDqYQirj3VA90giT5ndUcbxiUY70LXHGexXLciiUfKeosxWT%2A-UV7QjCYk;'},
+        //url: 'https://cas.sdo.com/cas/loginStateService?method=checkstat',
+        type: 'get',
+        success: after_getfeed
+        });
+
+};
+
 function doLogin() {
     var url = "https://dplogin.sdo.com/dispatchlogin.fcgi";
+    //var cookie_str = 'sdo_beacon_id=58.215.45.151.1302607175421.9; __utmz=237578050.1302607176.1.1.utmccn=(direct)|utmcsr=(direct)|utmcmd=(none); ver=1; __utma=237578050.48023496.1302607176.    1302607176.1304689726.2; SNDA_ADRefererSystem_MachineTicket=98cfe54b-c697-42ff-93bd-67afc31f9fa1; sto-id-20480=JMAKJGAKFAAA; sdo_dw_track="xHNvo2V0fNbE4/1D8QURAw==";              SDO_ACCOUNT_TYPE=';
+    //cookie += cookie_str;
     // Retrieve the values from the form elements
 
     var postStr = "";
@@ -49,6 +81,7 @@ function doLogin() {
             url: url,  
             type: 'post',
             data: postStr,
+            headers: { Cookie: cookie },
             success: after_login_sdo
         }); 
     }
@@ -58,7 +91,7 @@ function doLogin() {
 function after_login_sdo(responseText) { 
     check_cookie();
     this.responseText = responseText;
-    alert("after_login_sdo:responseText: "+this.responseText);
+    //alert("after_login_sdo:responseText: "+this.responseText);
     if(this.responseText == "password missed") { // wrong pass/user!
         alert("wrong username or password"); 
     } else { // Success!
@@ -72,11 +105,11 @@ function after_login_sdo(responseText) {
         var reg = /href = .*;/;
         var result =  reg.exec(this.responseText);
         eval("var href; "+ result);
-        alert(href);
+        //alert(href);
         ajax_handler = $.ajax({
             url: href, 
             type: 'get', 
-            headers: { Cookie: $.cookie() },
+            headers: { Cookie: cookie },
             success: after_login_sdo_href
         });
     }
@@ -86,23 +119,67 @@ function after_login_sdo_href(responseText) {
     check_cookie();
     this.responseText = responseText;
     //this.responseText = '<script language="javascript">\n document.location = "http://www.tuita.com/login?refer=&ticket=ST-c96505a7-9f18-4045-b817-2c43e6f021cb";    </script>';
-    alert("after_login_sdo_href:"+this.responseText);
-    alert("cookie:"+JSON.stringify(this));
-    var reg = /location = .*;/;
+    //alert("after_login_sdo_href:"+this.responseText);
+    var reg = /ticket=.*;/;
     var result =  reg.exec(this.responseText);
+    result = "var href=\"http://zdwalter.tuita.com/?"+result; //FIXME: any other page will cause 302 redirect, which loss cookie by $.ajax
     alert(result);
-    eval((""+result).replace('location','var href'));
+    eval(result);
+
     alert(href);
+    return;
     ajax_handler = $.ajax({
         url: href, 
         type: 'get', 
-        headers: { Cookie: $.cookie() },
-        success: after_login_tuita
+        headers: { Cookie: " tt_reg=1; tt_login=1; b_t_s=t104867177621xs; __utmz=143606314.1304867178.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=143606314.288821922.1304867178.1304867178.1304867178.1; __utmc=143606314; __utmb=143606314.3.10.1304867178" },
+        success: after_login_tuita,
     });
 }; //after_login_sdo_href
 
 function after_login_tuita(responseText) {
     check_cookie();
     this.responseText = responseText;
-    alert("after_login_tuita:"+this.responseText);
+
+    var reg = /__ttst=[^;]*;/;
+    var result = reg.exec(cookie);
+    alert('got __ttst:', result);
+
+    ajax_handler = $.ajax({
+        url: 'http://www.tuita.com/home/getfeed',
+        headers: { Cookie: result},
+        type: 'get',
+        success: after_getfeed
+        });
 }; //after_login_tuita
+
+function go_checkstat(responseText) {
+    check_cookie();
+    this.responseText = responseText;
+
+    alert("go_checkstat:"+this.responseText);
+    ajax_handler = $.ajax({
+        headers: { Cookie: cookie },
+        url: 'https://cas.sdo.com/cas/loginStateService?method=checkstat',
+        type: 'get',
+        success: after_getfeed
+        });
+
+};
+
+function after_checkstat(stat) {
+    alert(stat);
+    if (stat == "checkstat({'CAS_LOGIN_STATE':'1'})") {
+        alert('login success!');
+        ajax_handler = $.ajax({
+        url: 'http://www.tuita.com/home/getfeed',
+        type: 'get',
+        headers: { Cookie: cookie },
+        success: after_getfeed});
+    }
+  
+}; //after_checkstat
+
+function after_getfeed(feed) {
+    alert('feed:\n'+feed);
+    alert(ajax_handler.getAllResponseHeaders());
+};
