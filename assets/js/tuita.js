5e284f9eac2f290f09d43d1e1d34560461017152
Walter Zheng <zdwalter@gmail.com>
2011-05-10 16:05:25 +0800

    more type

--- assets/js/tuita.js
+++ assets/js/tuita.js
@@ -16,12 +16,38 @@ function load_tuita() {
 var tuita = {};
 
 var types = {
-    1: 'post',
-    2: '2',
-    7: 'repost',
+    1: 'tuita',
+    2: 'post_blog',
+    4: 'photoset',
+    7: 'reblog',
     21: 'unknown'
 };
 
+tuita.html = function(post_content) {
+    var post = '';
+    var type = post_content.post_type;
+    if (type == 'tuita') {
+        post += '<div>' + post_content.body + '</div>'
+            ;
+    }
+    else if (type == 'post_blog') {
+        post +=  '<h2 class="title">' + post_content.post_title + '</h2>'
+            +     '<div>' + post_content.summary + '</div>'
+            ;
+
+    }else if (type == 'photoset') {
+        for (i in post_content) {
+            var photo = post_content[i];
+            post += '<div>' + photo.photo_url + "." + photo.photo_type +"</div>";
+        }
+    } else if (type == 'reblog') {
+        post += '<div>' + post_content.reblog_text + '</div>';
+        post += tuita.html(post_content.reblog_body);
+    } else {
+        post += '<div>' + post_content + '</div>';
+    }
+    return post;
+};
 tuita.parse = function(feed) {
     try {
         var typeid = feed.typeid;
@@ -30,20 +56,17 @@ tuita.parse = function(feed) {
         var div = '<div>';
         var avatar = feed.sdid;
         var content = feed.content;
-        if (type == 'post') {
-            var poster = content.sdid;
-            var post_content = content.post_content;
-            var post_type = type.post_type; //tuita, reblog
-            var summary = post_content.summary;
-            var post = 
-                  '<div class="feed_group">'
-                +   '<div class="feed">'
-                +     '<p class="cell_user_info">' + avatar.blog_title + '</p>'
-                +     '<div>' + summary + '</div>'
+        var poster = content.sdid;
+        var post_content = content.post_content;
+        var post = '<div class="feed_group">'
+                    +  '<p class="cell_user_info">' + avatar.blog_title + '</p>'
+                    +  '<div class="content">' 
+                    +    '<div class="feed">';
+        post += tuita.html(post_content);
+        post    +=     '</div>'
                 +   '</div>'
                 + '</div>';
-            div += post;
-        };
+        div += post;
 
         div += '</div>';
         x$('article#feeds').bottom(div);
@@ -76,33 +99,3 @@ function after_load_tuita(res) {
     tuita.render(eval(feeds));
 };
 
-
-var test = (function() {
-    var feed = {"sdid":    {"avatar_thumb_big":"http:\/\/ft.staticsdo.com\/bf\/dd\/1011627165\/64x64.jpg?1","avatar_thumb_small":"http:\/\/ft.staticsdo.com\/bf\/dd\/1011627165\/20x20.jpg?1",            "prime_blog_id":"1011627165","full_domain":"ooooo.tuita.com","blog_title":"\u8499\u8499","sdid":1011627165}
-    ,"appid":10001,"typeid":7,"fid":"02000000000138f80000012fd815a925","time":1305000388901,
-    "content":
-    {"post_id":"35566",
-    "sdid":
-    {"avatar_thumb_big":"http:\/\/ft.staticsdo.com\/d1\/87\/1546495719\/64x64.jpg?1","avatar_thumb_small":"http:\/\/ft.staticsdo.com\/d1\/87\/1546495719\/20x20.jpg?1",        "prime_blog_id":"1546495719","full_domain":"simonyang2008.tuita.com","blog_title":"\u6768\u5c11\u5f6c","sdid":"1546495719"}
-    ,"post_author":"1011627165","post_title":"","post_type":"reblog","post_tag":"","post_blog":"1011627165","post_content":
-    {"reblog_id":"35559","reblog_text":"\u5e2e\u8f6c\u3001\u3001","reblog_body":
-    {"post_id":"35559","post_author":"1546495719","post_title":"\u8d21\u732e3\u679a\u63a8\u4ed6\u9080\u8bf7\u7801","post_type":"blog","post_tag":"\u9080\u8bf7\u7801",     "post_blog":"1546495719","post_content":
-    {"summary":"\u5728\u63a8\u4ed6\u6536\u83b7\u4e863\u679a\u9080\u8bf7\u7801\uff0c\u53cb\u60c5\u8d60\u9001\uff1ahttp:\/\/www.tuita.com\/invite\/DQlGUjPyhttp:\/\/www.tuita.com\/invite\/wp898ugxhttp:\/\/www.tuita.com\/invite\/zwcHmAab","suInfo":false,"suImg":false,"suHtml":                                                                        
-    "<div>\u5728\u63a8\u4ed6\u6536\u83b7\u4e863\u679a\u9080\u8bf7\u7801\uff0c\u53cb\u60c5\u8d60\u9001\uff1a<\/div><div>http:\/\/www.tuita.com\/invite\/DQlGUjPy<\/div><div>http:\/\/   www.tuita.com\/invite\/wp898ugx<\/div><div>http:\/\/www.tuita.com\/invite\/zwcHmAab<\/div>","body":                                                                                
-    "<div>\u5728\u63a8\u4ed6\u6536\u83b7\u4e863\u679a\u9080\u8bf7\u7801\uff0c\u53cb\u60c5\u8d60\u9001\uff1a<\/div><div>http:\/\/www.tuita.com\/invite\/DQlGUjPy<\/div><div>http:\/\/www.tuita.com\/invite\/wp898ugx<\/div><div>http:\/\/www.tuita.com\/invite\/zwcHmAab<\/div>"}
-    ,"post_time":1305000318,"post_source":"web","post_ip":3683507026}
-    }        ,"post_time":1305000388,"post_source":"web","post_ip":1996904574,"stat":        {"post_forward":0,"post_collection":0,"post_comment":0}        ,"like_flag":false}};
-
-    var feed_1 = {"sdid":
-{"avatar_thumb_big":"http:\/\/ft.staticsdo.com\/49\/49\/1534645712\/64x64.jpg?2","avatar_thumb_small":"http:\/\/ft.staticsdo.com\/49\/49\/1534645712\/20x20.jpg?2",            "prime_blog_id":"1534645712","full_domain":"smilodon.tuita.com","blog_title":"\u84dd\u7ffc","sdid":1534645712}
-    ,"appid":10001,"typeid":1,"fid":"02000000000137ef0000012fd7beedfc","time":1304994704892,"content":
-        {"post_id":"35048","sdid":"","post_author":"1534645712","post_title":"","post_type":"tuita","post_tag":"","post_blog":"1534645712","post_content":
-                {"summary":"\u5565\u65f6\u5019\u80fd\u6709\u5220\u8bc4\u8bba\u7684\u529f\u80fd\uff1f","body":"\u5565\u65f6\u5019\u80fd\u6709\u5220\u8bc4\u8bba\u7684\u529f\u80fd\uff1f"}
-                        ,"post_time":1304994704,"post_source":"web","post_ip":1917879559,"stat":
-                                {"post_forward":0,"post_collection":0,"post_comment":2}
-                                        ,"like_flag":false}
-};
-
-    //tuita.parse(feed_1);
-
-})();
